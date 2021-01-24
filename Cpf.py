eaba87a4d775e6fec6542e02aea49fb68316ebdb
Gitlab.network <roque.souza@portodigital.pt>
2021-01-24 02:52:30 +0000

    update cpf

--- Cpf.py
+++ Cpf.py
@@ -1,33 +1,26 @@
+from validate_docbr import CPF
+
+
 class Cpf:
-    
     def __init__(self, documento):
         documento = str(documento)
         if self.cpf_eh_valido(documento):
-            self.cpf = documento
-            
+            self.cpf = documento    
         else:
             raise ValueError("Cpf inválido!!")
         
-    def __str__(self) -> str:
-        return self.format_cpf()
     
-    
-    def cpf_eh_valido(self, documento):
-        if len(documento) == 11:
-            return True
+    def cpf_eh_valido(self, cpf):
+        if len(cpf) == 11:
+            validador = CPF()
+            return validador.validate(cpf)
         else:
-            return False
+            raise ValueError("Quantidade de digitos invalida")
         
     def format_cpf(self):
-        fatia_um = self.cpf[:3]
-        fatia_dois = self.cpf[3:6]
-        fatia_tres = self.cpf[6:9]
-        fatia_quatro = self.cpf[9:]
-
-        return (
-            "{}.{}.{}-{}".format(
-            fatia_um, 
-            fatia_dois,
-            fatia_tres,
-            fatia_quatro
-        ))
+        mascara = CPF()
+        return mascara.mask(self.cpf)
+    
+    def __str__(self):
+        return self.format_cpf()
+    
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from Cpf import Cpf
 
-cpf = "12345678901"
-objecto_cpf = Cpf(cpf)
+cpf_um = Cpf("90587243520")
 
-print(objecto_cpf)
-
+print(cpf_um)
